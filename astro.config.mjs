f326531a6dcdd09f4209ef9b3fb5cdea8b896516
localhost <o0l2qp3e@duck.com>
2023-07-02 18:14:06 +0200

    moving to vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,13 @@
 import { defineConfig } from 'astro/config';
 import node from "@astrojs/node";
+import vercel from "@astrojs/vercel/serverless";
 
+// https://astro.build/config
 export default defineConfig({
   output: 'server',
   server: {
     port: 80,
     host: true
   },
-  adapter: node({
-    mode: "standalone"
-  })
+  adapter: vercel()
 });
